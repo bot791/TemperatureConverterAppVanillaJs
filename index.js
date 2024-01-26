1551d6d00400fe3fd20bc0fa52a461ba28d8d527
kajal biswas <pikb2015@gmail.com>
2024-01-26 12:31:54 +0530

    vanilla convert temp app

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-document.getElementById("count").innerText = 5 + 5;
+const textBox = document.getElementById("textBox");
+const toFahrenheit = document.getElementById("toFahrenheit");
+const toCelcius = document.getElementById("toCelcius");
+const result = document.getElementById("result");
+
+let temp;
+
+const convert = () => {
+  if (toFahrenheit.checked) {
+    temp = Number(textBox.value);
+    temp = (temp * 9) / 5 + 32;
+    result.textContent = `${temp.toFixed(1)} degree Fahrenheit  `;
+  } else if (toCelcius.checked) {
+    temp = Number(textBox.value);
+    temp = temp - (32 * 5) / 9;
+    result.textContent = `${temp.toFixed(1)} degree Celcius`;
+  } else {
+    result.textContent = "select a unit";
+  }
+};
